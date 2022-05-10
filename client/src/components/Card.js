b42cd8518c083136cbd562a1dc2151064892f0e6
su-lim <87872237+su-lim@users.noreply.github.com>
2022-05-10 09:48:09 +0900

    Add Card Components

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,12 +1,65 @@
-import React from 'react';
-import '../styles/Card.css';
-// import { connect } from 'react-redux';
+import React, {useState} from 'react';
+import './Card.css';
+import fes_data from './card_sample.js';
+import * as RiIcons from 'react-icons/ri';
+import { IconContext } from 'react-icons';
 
 function Card(){
+
+    let [fes, set_fes] = useState(fes_data);
+
     return (
-        <div>
-            aa
+        <div class="outer">
+            <div class="div-card">
+            {
+                fes.map(function(fes_data, i){
+                    return <Item fes={fes[i]} idx={fes[i].id} key={i} />
+                })
+            }
+            </div>
         </div>
     )
 }
+
+function Item(props){
+    return (
+        <div class="card-inner" onClick={()=>{alert('a')} }>
+            <IconContext.Provider value={{ color: 'gray' }}>
+            <img class="card-img" src={props.fes.img}/>
+            <div class="card-info">
+                <div class="card-info-left">
+                    <span class="card-title">{props.fes.title}</span>
+                    <div class="card-info-inner">
+                        <RiIcons.RiMapPin2Fill class=""/>
+                        <span class="card-map card-text">{props.fes.address4} |</span>
+                        <span class="card-organizer card-text">{props.fes.organizer}</span>
+                    </div>
+                    
+                        {
+                            props.fes.hashtag.map(function(hash, i){
+                                return (
+                                    <span class="card-hashtag">
+                                        {"#"+hash}
+                                    </span>
+                            )})
+                        }
+                    
+                </div>
+                <div class="card-info-right">
+                    <div>
+                        <span class="card-info-love">{"♡ 0"}</span>
+                    </div>
+                    <div>
+                        <span class="card-price">예상 예약가 {props.fes.price}원</span>
+                    </div>
+                    
+                    
+                </div>
+            </div>
+            </IconContext.Provider>
+        </div>
+    )
+
+}
+
 export default Card;
---
+++ client/src/components/card_sample.js
@@ -0,0 +1,40 @@
+export default [
+    {
+      id : 0,
+      title : "화성행궁 야간개장",
+      address1 : "경기도",
+      address2 : "수원시",
+      address3 : "팔달구",
+      address4 : "화성행궁",
+      organizer : "수원문화재단",
+      hashtag : ['명소','야경','관광'],
+      price : 0,
+      img: "/img/item/card_park.jpg"
+    },
+  
+    {
+        id : 1,
+        title : "경복궁 야간개장",
+        address1 : "서울특별시",
+        address2 : "종로구",
+        address3 : "교북동",
+        address4 : "경복궁",
+        organizer : "경복궁관리소",
+        hashtag : ['명소','야경','관광'],
+        price : 1000,
+        img: "/img/item/card_park.jpg"
+      },
+  
+      {
+        id : 2,
+        title : "천안흥타령춤축제",
+        address1 : "충청남도",
+        address2 : "천안시",
+        address3 : "서북구",
+        address4 : "천안종합운동장",
+        organizer : "천안문화재단",
+        hashtag : ['축제','천안12경','춤'],
+        price : 0,
+        img: "/img/item/card_park.jpg"
+      },
+  ] 
